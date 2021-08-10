15a2b9931c8d7abda858d68c076ca833e5f516f2
Eugeniy Galkovskiy <zhegal@gmail.com>
2021-08-10 23:34:53 +0300

    javascript

--- src/4/9/4.9.js
+++ src/4/9/4.9.js
@@ -1,10 +1,27 @@
+const citySuffixes =
+[
+    'де', 'на', 'лез', 'ла', 'сюр',
+];
+
 function capitalize( cityName ) {
-    let cityNameFirst = cityName[0].toUpperCase();
-    let cityNameCapitalized = cityName.toLowerCase();
-    cityNameCapitalized = cityNameFirst + cityNameCapitalized.substring( 1 );
-    return cityNameCapitalized;
+    return cityName
+        .toLowerCase()
+        .split( '-' )
+        .map(
+            ( chunk ) =>
+            {
+                if( citySuffixes.includes( chunk ) ) {
+                    return chunk;
+                }
+                return chunk.substring( 0, 1 ).toUpperCase() + chunk.substring( 1 );
+            }
+        )
+        .join( '-' );
 }
 
-let cityNameCapitalized = capitalize('сЕН-сатЮрнЕн-лЕз-АПт');
-
-console.log( cityNameCapitalized );
+console.log( capitalize('кИЇв') );
+console.log( capitalize('нЬЮ-йорк') );
+console.log( capitalize('лос-анджелес') );
+console.log( capitalize('рІо-дЕ-жАнейРо') );
+console.log( capitalize('фрАнуФУрт-На-мАйні') );
+console.log( capitalize('сЕН-сатЮрнЕн-лЕз-АПт') );
