3f3b56f948957d77ca80f7a293da7acd7328ca1f
Eugeniy Galkovskiy <zhegal@gmail.com>
2021-08-15 14:28:23 +0300

    7.6

--- src/7/6/index.php
+++ src/7/6/index.php
@@ -1,1 +1,19 @@
 <?php
+
+if ($file = getCities()) {
+    $row = fgetcsv( $file );
+    fclose( $file );
+}
+ 
+// functions
+
+function getCities()
+{
+    $filepath = __DIR__ . '/../../data/cities.csv';
+
+    if (file_exists($filepath)) {
+        return fopen( $filepath, 'r' );
+    }
+    
+    return false;
+}
