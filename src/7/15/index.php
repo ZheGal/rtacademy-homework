4cca64d6603484827ab49164774f48d5737f2298
Eugeniy Galkovskiy <zhegal@gmail.com>
2021-09-23 18:27:24 +0300

    clean main branch

--- src/7/15/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-    $link = '/blog';
-    echo "Please go to <a href='{$link}'>{$link}</a>";
-?>
--- src/7/tests/test4.php
+++
@@ -1,9 +0,0 @@
-<?php
-header("Content-type: text/plain");
-
-$get = $_GET['get'] ?? 'Null';
-$getA = isset($_GET['get']) ? $_GET['get'] : 'Null A';
-
-echo $get;
-echo "\n";
-echo $getA;
