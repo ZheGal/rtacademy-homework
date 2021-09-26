1e1d5ef671be499ea9e95e92dbb2ddb27993f429
Eugeniy Galkovskiy <zhegal@gmail.com>
2021-09-26 09:56:31 +0300

    move files from old branch

---
+++ src/11/classes/ItemInterface.php
@@ -0,0 +1,22 @@
+<?php
+
+interface ItemInterface
+{
+    public function getId(): int;
+    public function setId(int $id);
+    public function getTitle(): string;
+    public function setTitle(string $title);
+    public function getAlias(): string;
+    public function setAlias(string $alias);
+    public function getDescription(): string;
+    public function setDescription(string $desc);
+    public function getAuthor(): Author;
+    public function setAuthor(Author $author);
+    public function getPublishDate(): string;
+    public function setPublishDate(string $date);
+    public function getCategory(): Category;
+    public function setCategory(Category $category);
+    public function getCover(): Cover;
+    public function setCover(Cover $cover);
+    public function getUrl(): string;
+}
