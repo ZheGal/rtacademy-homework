f66d82acba7f1184b91a63b1935aedfec44fd5f0
Eugeniy Galkovskiy <zhegal@gmail.com>
2021-09-27 10:54:38 +0300

    sql 5.2

---
+++ users_202109271043.sql
@@ -0,0 +1,4 @@
+INSERT INTO helloworld.users (id,lastname,firstname,middlename,birthday,email,phone,gender) VALUES
+	 (1,'Назаров','Семен','Демидович','1991-10-09','naza@mail.com','+380617892239','male'),
+	 (2,'Кузнєцова','Анна','Володимирівна','1994-03-09','kuznica@mail.com','+380541374149','female'),
+	 (3,'Шестакова','Софія','Андріївна','1989-01-05','shestack@mail.com','+380141578931','female');
