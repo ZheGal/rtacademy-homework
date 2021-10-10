bd6bfbf9acb5768d895828fa9e3a430315dbf1a7
Eugeniy Galkovskiy <zhegal@gmail.com>
2021-10-10 11:42:32 +0300

    7-25 gets and create init

--- src/7/25/lib/controllers/CategoryApiController.php
+++ src/7/25/lib/controllers/CategoryApiController.php
@@ -9,7 +9,21 @@ class CategoryApiController extends CategoryController
         try {
             $model = new \lib\models\CategoriesModel();
             $categories = $model->getList();
-            return $categories;
+            
+            $response = [
+                'status' => 200,
+                'data' => []
+            ];
+
+            foreach ($categories as $category) {
+                $response['data'][] = [
+                    'id' => $category->getId(),
+                    'title' => $category->getTitle(),
+                    'alias' => $category->getAlias()
+                ];
+            }
+            return $response;
+
         } catch ( \Exception $e ) {
             return [
                 'status' => 490,
@@ -20,22 +34,86 @@ class CategoryApiController extends CategoryController
 
     public function getSingle( int $id ) : array
     {
-        return [];
+        try {
+            $model = new \lib\models\CategoriesModel();
+            $single = $model->getSingle( $id );
+            
+            $response = (!empty($single)) ? [
+                'status' => 200,
+                'data' => [
+                    'id' => $single->getId(),
+                    'title' => $single->getTitle(),
+                    'alias' => $single->getAlias()
+                ]
+            ] : [
+                'status' => 404,
+                'error' => 'Категорію не знайдено'
+            ];
+            return $response;
+        } catch( \Exception $e ) {
+            return [
+                'status' => 490,
+                'error' => "Помилка БД: {$e->getMessage()}"
+            ];
+        }
     }
 
     public function create() : array
     {
-        print_r($_POST);
-        return [];
+        try {
+            $model = new \lib\models\CategoriesModel();
+
+            $title = $_POST['title'] ?? '';
+            $alias = $_POST['alias'] ?? '';
+
+            if (!$this->_validateTitle($title)) {
+                $response = [
+                    'status' => 411, 
+                    'error' => 'Необхідно заповнити поле "Назва"'
+                ];
+            }
+
+            if (!$this->_validateAlias($alias)) {
+                $response = [
+                    'status' => 412, 
+                    'error' => 'Необхідно заповнити поле "Аліас"'
+                ];
+            }
+
+            return [];
+
+        } catch( \Exception $e ) {
+            return [
+                'status' => 490,
+                'error' => "Помилка БД: {$e->getMessage()}"
+            ];
+        }
     }
 
     public function update( int $id ) : array
     {
-        return [];
+        try {
+            $model = new \lib\models\CategoriesModel();
+            $single = $model->getSingle( $id );
+
+        } catch( \Exception $e ) {
+            return [
+                'status' => 490,
+                'error' => "Помилка БД: {$e->getMessage()}"
+            ];
+        }
     }
 
     public function delete( int $id ) : array
     {
-        return [];
+        try {
+            $model = new \lib\models\CategoriesModel();
+
+        } catch( \Exception $e ) {
+            return [
+                'status' => 490,
+                'error' => "Помилка БД: {$e->getMessage()}"
+            ];
+        }
     }
 }
